e3fd8825da800ceb05c71ca248a775938af24259
Ayobami Elutade <53827536+Ayobamee@users.noreply.github.com>
2020-11-02 13:33:00 +0100

    Updated elements for Create customer page

---
+++ cypress/pageObjects/CreateCustomerPage.js
@@ -0,0 +1,49 @@
+class CustomerPage
+{
+    
+
+getCustomers()
+
+{
+
+    return  cy.get(':nth-child(1) > .sc-qPzgd > :nth-child(1)')
+}
+
+getaddCustomer() 
+{
+    return cy.get('.sc-fzqzlV')
+
+}
+
+getCustomerName()
+{
+    return cy.get(':nth-child(1) > .sc-fzpjYC')
+}
+
+getCustomerPhoneNo()
+
+{
+    return cy.get(':nth-child(2) > .sc-fzpjYC')
+
+}
+
+
+getSelectAddedCustomer()
+
+{
+
+    return cy.get(':nth-child(1) > .sc-pKMan')
+}
+
+
+
+getAddCustomerButton() 
+{
+    return cy.get('.dSeJSB')
+}
+
+}
+
+
+
+export default CustomerPage;
